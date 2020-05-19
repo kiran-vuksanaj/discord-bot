b449516af7bfffe8a10294208f12522ecbf58427
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-05-19 14:04:55 -0400

    eliminating spotipy

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ if [[ -f ".secret_keys" ]]; then
 	echo "Reading Keys from ./.secret_keys...";
 	keys=(`cat .secret_keys`);
 	export DISCORD_TOKEN=${keys[1]};
-	export SPOTIPY_CLIENT_ID=${keys[3]};
-	export SPOTIPY_CLIENT_SECRET=${keys[5]};
-	export SPOTIPY_REDIRECT_URI=${keys[7]};
+	export SPOTIFY_CLIENT_ID=${keys[3]};
+	export SPOTIFY_CLIENT_SECRET=${keys[5]};
+	export SPOTIFY_REDIRECT_URI=${keys[7]};
 	echo "Successfully exported keys to environment.";
 else
 	echo "./.secret_keys not found. Please enter keys.";
@@ -14,16 +14,16 @@ else
 	echo "discord: $DISCORD_TOKEN" >> .secret_keys;
 	echo -n "Spotify Client ID: ";
 	read ans;
-	export SPOTIPY_CLIENT_ID=$ans;
-	echo "spotify_id: $SPOTIPY_CLIENT_ID" >> .secret_keys;
+	export SPOTIFY_CLIENT_ID=$ans;
+	echo "spotify_id: $SPOTIFY_CLIENT_ID" >> .secret_keys;
 	echo -n "Spotify Client Secret: ";
 	read ans;
-	export SPOTIPY_CLIENT_SECRET=$ans;
-	echo "spotify_secret: $SPOTIPY_CLIENT_SECRET" >> .secret_keys;
+	export SPOTIFY_CLIENT_SECRET=$ans;
+	echo "spotify_secret: $SPOTIFY_CLIENT_SECRET" >> .secret_keys;
 	echo -n "Spotify Redirect URI: ";
 	read ans;
-	export SPOTIPY_REDIRECT_URI=$ans;
-	echo "spotify_uri: $SPOTIPY_REDIRECT_URI" >> .secret_keys;
+	export SPOTIFY_REDIRECT_URI=$ans;
+	echo "spotify_uri: $SPOTIFY_REDIRECT_URI" >> .secret_keys;
 	echo "Keys exported to environment and ./.secret_keys";
 fi
 
