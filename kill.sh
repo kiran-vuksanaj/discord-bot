a6a253afb04c7659b9928b43ec3fd1b9fc2d8df9
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-05-21 00:07:41 +0000

    poll loop until process completes

--- kill.sh
+++ kill.sh
@@ -2,7 +2,15 @@ if [[ -f "logs/live.txt" ]]; then
 	PID=(`head -n 1 logs/live.txt`)
 	echo "Client Alive at pid $PID"
 	kill $PID
-	echo "Client Killed"
+	echo "Sent Kill Signal"
+	# cycle check whether process is alive
+	kill -0 $PID
+	while [[ $? == 0 ]]; do
+	    sleep 1
+	    kill -0 $PID &> /dev/null
+	done;
+	
+	echo "Client Dead"
 else
 	echo "No client currently alive"
 fi
